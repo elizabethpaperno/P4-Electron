f6eb6eb096203b50c34273469d53bc66586e2d05
JefferyTang23 <jtang30@stuy.edu>
2023-05-05 13:20:28 -0400

    login/signup

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask import render_template
 from flask import request          
 from flask import session           
 from flask import redirect, url_for 
+from auth import *
 
 app = Flask(__name__) #create instance of class Flask
 
@@ -21,3 +22,27 @@ def hello_world():
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+
+@app.route('/login_user', methods = ["POST"])
+def login():
+    if checkCreds(request.form['username'], request.form['password']):
+        return redirect("main")
+    
+
+@app.route('/signup', methods = ["GET", "POST"])
+def show_signup():
+    print("hi")
+    return render_template('signup.html')
+
+@app.route('/create_user', methods = ["POST"])
+def create_user():
+    username = request.form['username']
+    password = request.form['password']
+    if checkUsernameAvailability(username):
+        addNewUser(username, password)
+    else:
+        return render_template('signup.html', error = "Username already exists.")
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
