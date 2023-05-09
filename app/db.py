ab5e2aaa8c8a896d602ee76f8e41711d41d7cc8b
JefferyTang23 <jtang30@stuy.edu>
2023-05-08 22:44:05 -0400

    json start

--- app/db.py
+++ app/db.py
@@ -6,7 +6,7 @@ def get_connection(db):
 
 
 def query_usersdb(query, args=(), all=False):
-    conn = get_connection("users.db")
+    conn = get_connection("P4.db")
 
     with conn:
         cur = conn.cursor()
@@ -15,3 +15,11 @@ def query_usersdb(query, args=(), all=False):
     conn.close()
 
     return (r[0] if r else None) if not all else r
+
+DB_FILE = "P4.db"
+db_name = "P4.db"
+db = sqlite3.connect(DB_FILE, check_same_thread = False)
+c = db.cursor()
+
+c.execute("""CREATE TABLE IF NOT EXISTS users(username TEXT UNIQUE, password TEXT, r_saved TEXT, r_visited TEXT, reviews TEXT, f_cat LIST, location TEXT, b_pref DICT, a_pref BOOL, s_pref TEXT, d_res LIST)""")
+c.execute("""CREATE TABLE IF NOT EXISTS retuarants(name TEXT, address TEXT, takeout BOOL, parking BOOL, cat LIST, hours DICT, reviews DICT, alcohol BOOL, s_grade TEXT, seating TEXT, diet LIST, u_reviews LIST)""")
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,1 +1,16 @@
-console.log("hi")
+import alcohol from './datasets/alcohol.json' assert {type: "json"}
+//const sqlite3 = require('sqlite3').verbose();
+
+console.log("hi");
+console.log(alcohol);
+console.log(alcohol.meta.view)
+
+//const sqlite3 = require('sqlite3').verbose();
+
+// let db = new sqlite3.Database('./../../../restuarants.db', (err) => {
+//     if (err) {
+//       console.error(err.message);
+//     }
+//     console.log('Connected to the restuarants database.');
+//   });
+
