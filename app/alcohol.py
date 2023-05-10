587bc5ccbd261854f6aa25014df96ffd94b5ef8b
JefferyTang23 <jtang30@stuy.edu>
2023-05-10 13:44:57 -0400

    get + address

--- app/alcohol.py
+++ app/alcohol.py
@@ -4,8 +4,27 @@ alcohol = open('app/datasets/alcohol.json')
 
 data = json.load(alcohol)
 
-print(data)
+#print(data)
 
 print(data['data'][0])
 
+p_name = []
+mode = []
+address = []
+
+for res in data['data']:
+    p_name.append(res[13])
+    mode.append(res[22])
+    address.append(str(res[15]) + ' ' + str(res[19]))
+
+print(address)
 alcohol.close()
+
+def get_name():
+    return p_name
+
+def get_mode():
+    return mode
+
+def get_address():
+    return address
--- app/sanitation.py
+++ app/sanitation.py
@@ -4,16 +4,30 @@ sanitation = open('app/datasets/sanitation.json')
 
 data = json.load(sanitation)
 
-#print(data['data'][0][17])
+print(data['data'][0])
 
 violations = []
 dba = []
 grade = []
+address = []
 
 for res in data['data']:
-    violations.append(res[14])
-    dba.append(res[9])
-    grade.append(res[17])
+    violations.append(str(res[14]))
+    dba.append(str(res[9]))
+    grade.append(str(res[17]))
+    address.append(str(res[10]) + ' ' + str(res[11]) + ' ' + str(res[12]))
 
-print(violations)
+print(address)
 sanitation.close()
+
+def get_violations():
+    return violations
+
+def get_dba():
+    return dba
+
+def get_grade():
+    return grade
+
+# def get_address():
+#     return address
