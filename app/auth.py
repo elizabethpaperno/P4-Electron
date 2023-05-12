3697fc62a380594b9364baf0ea62fb09920f1b5e
Elizabeth Paperno <liza.paperno@gmail.com>
2023-05-11 22:55:52 -0400

    add files

--- app/auth.py
+++ app/auth.py
@@ -1,34 +1,35 @@
-try:
-    from db import query_usersdb
-except:
-    from db import query_usersdb
-
+#try:
+    #from db import query_db
+#except:
+    #from db import query_db
+import db
 #add other cols later
 def createUsersTable(): 
-    query_usersdb("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
+    #db.query_db("DROP TABLE IF EXISTS users;")
+    db.query_db("CREATE TABLE IF NOT EXISTS users (username TEXT PRIMARY KEY, password TEXT)")
 
 def addNewUser(username, password): 
-    query_usersdb("INSERT INTO users VALUES (?, ?);", (username, password))
+    db.query_db("INSERT INTO users VALUES (?, ?);", (username, password))
 
 def checkUsernameAvailability(username):
-    user = query_usersdb("SELECT * FROM users WHERE username = ?", (username,))
+    user = db.query_db("SELECT * FROM users WHERE username = ?", (username,))
     return user is None
 
 def checkCreds(username, password):
-    user = query_usersdb("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+    user = db.query_db("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
     return user is not None
 
-def update_user_password(username, password):
-    query_usersdb("UPDATE users SET password = ? WHERE username = ?", (username, password))
+def updateUserPassword(username, password):
+    db.query_db("UPDATE users SET password = ? WHERE username = ?", (username, password))
 
-def delete_user(username):
-    query_usersdb("DELETE FROM users WHERE username = ?", (username))
+def deleteUser(username):
+    db.query_db("DELETE FROM users WHERE username = ?", (username))
 
-def get_user_password(username):
-    password = query_usersdb("SELECT password FROM users WHERE username = ?", (username,))
+def getUserPassword(username):
+    password = db.query_db("SELECT password FROM users WHERE username = ?", (username,))
     return password
     
-# LINES BELOW ONLY GET RUN IF "EXPLICITY RAN" with `python app/db/auth.py`
+# LINES BELOW ONLY GET RUN IF "EXPLICITY RAN" with `python app/auth.py`
 if __name__ == "__main__":
     create_user_info_table()
     print(check_username_availability("epap"))
@@ -36,4 +37,3 @@ if __name__ == "__main__":
     print(check_creds("epap", "hi"))
     print(check_creds("epap", "hi2"))
     print(check_username_availability("epap"))
-    # print(get_zipcoe("epap"))
