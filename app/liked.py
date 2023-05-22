7fd0215d4d324c78fbd77a8bc85062accd183bd6
Elizabeth Paperno <liza.paperno@gmail.com>
2023-05-21 23:21:13 -0400

    add db for allowing users to save resturants

---
+++ app/liked.py
@@ -0,0 +1,29 @@
+import sqlite3
+try:
+    from db import query_db
+except:
+    from db import query_db
+
+def createLikedRestTable():
+    query_db("DROP TABLE IF EXISTS liked_rest;")
+    query_db("CREATE TABLE IF NOT EXISTS liked_rest(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, rest_name TEXT);")
+
+def addRestaurant(user,rest_name):
+    query_db("INSERT INTO liked_rest (username, rest_name) VALUES (?,?);", (user, rest_name))
+
+def getListLikedRestaurants(user):
+    conn = sqlite3.connect("P4.db")
+    cur = conn.cursor()
+    unformatted = cur.execute('SELECT rest_name FROM liked_rest WHERE username = ?;', (user, )).fetchall()
+    #unformatted = query_db('SELECT rest_name FROM liked_rest WHERE username = ?;', (user, ))
+    conn.close()
+    formatted = []
+    for i in unformatted:
+        formatted.append(i[0])
+    return formatted
+
+if __name__ == "__main__":
+    createLikedRestTable()
+    addRestaurant("epap", "filler1")
+    addRestaurant("epap", "filler2")
+    getLikedRestaurants("epap")
