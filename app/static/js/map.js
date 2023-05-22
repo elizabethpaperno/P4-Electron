a994040102590fbff0ef7fada981a03818712879
Elizabeth Paperno <liza.paperno@gmail.com>
2023-05-21 22:35:06 -0400

    add new table func

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -7,22 +7,22 @@ const { Map, InfoWindow } = await google.maps.importLibrary("maps");
 const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary(
   "marker"
 );
-  
-  
+
+
   const newYork = { lat: 40.7128, lng: -74.0060}; // nyc is center of the map
   //const myLatLng1 = { lat: 40.7278, lng: -74.0138 };
   //const myLatLng2 = { lat: 40.7378, lng: -74.0138 };
   //const school = "345 Chambers St, New York, NY 10282"
   geocoder = new google.maps.Geocoder();
-  
+
   var mapOptions = { //holds map center
     zoom: 10,
     center: newYork
   }
-  
+
   map = new google.maps.Map(document.getElementById('map'), mapOptions); // create map
   map.setOptions({ styles: styles["hide"] }); //hide annoying map features
-  
+
   //var a = codeAddress(school)
   //a.addListener("click", blah);
   //console.log(a.getClickable())
@@ -34,7 +34,7 @@ var createWidget = (title) => {
   // const node = document.createTextNode("Onclick works");
   // widgetArea.appendChild(node)
   // //this was a test and it works
-  // //const cardParent = 
+  // //const cardParent =
   console.log(title)
 }
 
@@ -60,7 +60,7 @@ function codeAddress(data) {
       //console.log(a)
       //a.setClickable(true)
       return a;
-      
+
     } else {
       console.log('Geocode was not successful for the following reason: ' + status);
     }
@@ -109,36 +109,84 @@ window.addEventListener("load", (event) => {
   var list = addresses.split("rsuf")
   //console.log(list)
   var finalList= []
-  
+
   for (var i = 0; i < list.length; i++){
     restaurant = list[i]
     meta = restaurant.split("!")
     finalList.push(meta)
   }
   console.log(finalList)
-  
+
   //console.log(list)
-  
+
   //var i = 0;
-  
+
   var stop = 100;
-  
+
   if (list.length < 100) {
     stop = list.length
   }
-  
+
   for (var i = 0; i < stop; i++) {
     codeAddress(finalList[i]);
     sleep(1000);
     //console.log(list[i])
   }
-  
+
   // list.forEach(address => {
   //     codeAddress(address)
   //     sleep("1000")
-      
+
   // });
 });
+
+const sqlite3 = require('sqlite3').verbose();
+
+function query(sqlstr) {
+  let db = new sqlite3.Database('P4-Electron/P4.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the liked resturant database.');
+  });
+  let sql = sqlstr;
+  db.run(sql)
+  db.close((err) => {
+  if (err) {
+    console.error(err.message);
+  }
+    console.log('Close the database connection.');
+  });
+}
+
+def createLikedRestTable(){
+  query("CREATE TABLE IF NOT EXISTS liked_rest(username TEXT PRIMARY KEY, rest_name TEXT)")
+}
+
+def addRestaurant(user,rest_name){
+  query('INSERT INTO liked_rest VALUES (' + user + ', ' + rest_name ");")
+}
+
+def getLikedRestaurants(user){
+  list = []
+  let db = new sqlite3.Database('./P4.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the liked resturant database.');
+  });
+  let sql = 'SELECT rest_name FROM liked_rest WHERE user = ' + user + ';'
+  db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    list.push(row.res_name);
+    });
+  });
+  db.close();
+  console.log(list)
+}
 
 /*
 TODO: ADD the new categories
@@ -151,12 +199,12 @@ TODO: ADD the new categories
 //     const myLatLng1 = { lat: 40.7278, lng: -74.0138 };
 //     const myLatLng2 = { lat: 40.7378, lng: -74.0138 };
 //     const school = "345 Chambers St, New York, NY 10282"
-  
+
 //     const map = new google.maps.Map(document.getElementById("map"), {
 //       zoom: 10,
 //       center: newYork
 //     });
-  
+
 //     map.setOptions({ styles: styles["hide"] });
 
 //     new google.maps.Marker({
@@ -164,7 +212,7 @@ TODO: ADD the new categories
 //       map,
 //       title: "Hello World!",
 //     });
-  
+
 //   }
 
 // function addMarker(lat,long) {
