71643e03d20f16a155cca621e68dd04fe41d5422
Elizabeth Paperno <liza.paperno@gmail.com>
2023-05-22 21:06:57 -0400

    add alc + sanitation + sort functionality

--- app/reccomendations.py
+++ app/reccomendations.py
@@ -3,8 +3,9 @@ import yelp
 import db
 import match
 import pandas as pd
+import json
 
-#returns list formatted like [[rest_add1, score1], [rest_add2, score2]]
+#returns list formatted like [(rest_add1, score1), (rest_add2, score2)]
 def getListScoreAddresses(df, sa_data, username):
     list = []
     # hardcoded for now
@@ -25,18 +26,27 @@ def getListScoreAddresses(df, sa_data, username):
         score += len(common_cats) * cuisine_multiplier
         common_dietary = [c for c in cats if c in d_rest]
         score += len(common_dietary) * dietary_multiplier
-        if (a_pref == bool(match.getAlcohol(yelp.getShortAddress(df,address))):
-            score += alcohol_multiplier
-        if (sa_data[yelp.getShortAddress(df,address)]["score"] >= s_pref):
-            score += sa_data[yelp.getShortAddress(df,address)]["score"]
+        try:
+            if (a_pref == bool(match.getAlcohol(yelp.getShortAddress(df,address),sa_data))):
+                score += alcohol_multiplier
+        except:
+            # print("no alc avail")
+            score += 0
+        try:
+            if (int(sa_data[yelp.getShortAddress(df,address)]["score"]) >= s_pref):
+                score += int(sa_data[yelp.getShortAddress(df,address)]["score"])
+        except:
+            # print("no san avail")
+            score += 0
         neighborhood = yelp.getNeighborhood(df, address)
         if neighborhood in location:
             score += location_multiplier
         if (score >= 100):
-            list.append([address, score])
-    return list
+            list.append((address, score))
+    return sorted(list, key=lambda address: address[1], reverse=True)
 
 if __name__ == "__main__":
     df = pd.read_json("yelp.json")
     df = yelp.editDF(df)
+    sa_data = json.load(open("sanitation_alcohol.json"))
     print(getListScoreAddresses(df, sa_data, "epap5"))
